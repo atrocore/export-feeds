e1e2a4b390fa579ab38b7b51dc0f65bcd884fc90
r.ratsun <r.ratsun@gmail.com>
2021-11-02 12:07:51 +0200

    .

--- app/ExportType/Simple.php
+++ app/ExportType/Simple.php
@@ -281,7 +281,7 @@ class Simple extends AbstractType
                 if (isset($rowData[$columnData['number']][$columnData['name']])) {
                     $resultRow[$pos] = $rowData[$columnData['number']][$columnData['name']];
                 } else {
-                    $resultRow[$pos] = $configuration[0]['nullValue'];
+                    $resultRow[$pos] = $configuration[0]['markForNotLinkedAttribute'];
                 }
             }
             $result['data'][] = $resultRow;
