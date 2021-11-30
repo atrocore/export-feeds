5b8930b2ff15b20540628b0e802f0b87ca86f017
r.ratsun <r.ratsun@gmail.com>
2021-11-30 14:42:11 +0200

    .

--- app/Migrations/V1Dot3Dot1.php
+++ app/Migrations/V1Dot3Dot1.php
@@ -32,7 +32,7 @@ class V1Dot3Dot1 extends Base
         $this->execute("UPDATE `export_feed` SET jobs_max=10 WHERE deleted=0");
         $this->execute("ALTER TABLE `export_job` ADD data MEDIUMTEXT DEFAULT NULL COLLATE utf8mb4_unicode_ci");
         $this->execute("ALTER TABLE `export_feed` ADD `limit` INT DEFAULT '2000' COLLATE utf8mb4_unicode_ci");
-        $this->execute("UPDATE `export_feed` SET limit=2000 WHERE deleted=0");
+        $this->execute("UPDATE `export_feed` SET `limit`=2000 WHERE deleted=0");
         $this->execute("ALTER TABLE `export_feed` ADD separate_job TINYINT(1) DEFAULT '0' NOT NULL COLLATE utf8mb4_unicode_ci");
         $this->execute("ALTER TABLE `export_job` ADD sort_order INT DEFAULT NULL COLLATE utf8mb4_unicode_ci");
         $this->execute("ALTER TABLE `export_job` ADD count INT DEFAULT '0' COLLATE utf8mb4_unicode_ci");
