f096fbb39bba632a01d8ece122346f8e51aed380
r.ratsun <r.ratsun@gmail.com>
2022-01-24 13:46:15 +0200

    .

---
+++ app/Acl/ExportJob.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Export Feeds
+ * Free Extension
+ * Copyright (c) AtroCore UG (haftungsbeschränkt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Export\Acl;
+
+use Espo\Core\AclPortal\Base;
+use Espo\Entities\User;
+use Espo\ORM\Entity;
+
+class ExportJob extends Base
+{
+    public function checkEntity(User $user, Entity $entity, $data, $action)
+    {
+        if (in_array($action, ['create', 'delete'])) {
+            $action = 'edit';
+        }
+
+        return $this->getAclManager()->checkEntity($user, $entity->get('exportFeed'), 'edit');
+    }
+}
+
