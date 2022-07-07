fec746b4e2d2a442e270e333a77aadaddd41fd9f
r.ratsun <r.ratsun@gmail.com>
2022-07-07 14:43:38 +0300

    .

---
+++ app/Migrations/V1Dot4Dot35.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Export Feeds
+ * Free Extension
+ * Copyright (c) AtroCore UG (haftungsbeschränkt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This software is not allowed to be used in Russia and Belarus.
+ */
+
+declare(strict_types=1);
+
+namespace Export\Migrations;
+
+use Treo\Core\Migration\Base;
+
+class V1Dot4Dot35 extends Base
+{
+    public function up(): void
+    {
+        $this->execute("ALTER TABLE `export_configurator_item` ADD value_modifier MEDIUMTEXT DEFAULT NULL COLLATE utf8mb4_unicode_ci");
+    }
+
+    public function down(): void
+    {
+        $this->execute("ALTER TABLE `export_configurator_item` DROP value_modifier");
+    }
+
+    protected function execute(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
+    }
+}
