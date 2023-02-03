2aeedf37e3a341ecf20345da02628e85a0e40116
r.ratsun <r.ratsun@gmail.com>
2023-02-03 09:47:18 +0200

    .

--- app/Migrations/V1Dot6Dot20.php
+++ app/Migrations/V1Dot6Dot20.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 namespace Export\Migrations;
 
 use Espo\Core\Exceptions\Error;
+use Espo\Core\Utils\Util;
 use Treo\Core\Migration\Base;
 
 class V1Dot6Dot20 extends Base
@@ -30,34 +31,44 @@ class V1Dot6Dot20 extends Base
     public function up(): void
     {
         $this->getPDO()->exec("UPDATE export_feed SET `language`='' WHERE `language`='mainLocale'");
-        $this->getPDO()->exec("UPDATE export_configurator_item SET locale='main' WHERE locale='' OR locale IS NULL OR locale='mainLocale'");
-        $this->getPDO()->exec("ALTER TABLE export_configurator_item CHANGE locale `language` VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
+        $this->exec("UPDATE export_configurator_item SET locale='main' WHERE locale='' OR locale IS NULL OR locale='mainLocale'");
+        $this->exec("ALTER TABLE export_configurator_item CHANGE locale `language` VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
 
         $records = $this->getPDO()->query(
             "SELECT eci.*, ef.data as ef_data FROM export_configurator_item eci INNER JOIN export_feed ef ON ef.id=eci.export_feed_id WHERE eci.deleted=0 AND ef.deleted=0 AND eci.type='Field'"
         )->fetchAll(\PDO::FETCH_ASSOC);
 
-        /** @var \Espo\Core\Utils\Metadata $metadata */
-        $metadata = (new \Espo\Core\Application())->getContainer()->get('metadata');
+        $languages = [];
+        if ($this->getConfig()->get('isMultilangActive')) {
+            $languages = $this->getConfig()->get('inputLanguageList', []);
+        }
 
-        foreach ($records as $record) {
-            $data = @json_decode($record['ef_data'], true);
-            if (empty($data) || empty($data['entity'])) {
-                continue;
-            }
+        foreach ($languages as $language) {
+            foreach ($records as $record) {
+                if (empty($record['name']) || !is_string($record['name']) || mb_strlen($record['name']) < 4) {
+                    continue;
+                }
 
-            $fieldDefs = $metadata->get(['entityDefs', $data['entity'], 'fields', $record['name']]);
-            if (empty($fieldDefs['multilangField'])) {
-                continue;
+                $suffix = ucfirst(Util::toCamelCase(strtolower($language)));
+
+                if ($suffix === mb_substr($record['name'], -4)) {
+                    $name = $this->getPDO()->quote(mb_substr($record['name'], 0, -4));
+                    $this->getPDO()->exec("UPDATE export_configurator_item SET `name`=$name, `language`='{$language}' WHERE id='{$record['id']}'");
+                }
             }
-            $this->getPDO()->exec(
-                "UPDATE export_configurator_item SET `name`='{$fieldDefs['multilangField']}', `language`='{$fieldDefs['multilangLocale']}' WHERE id='{$record['id']}'"
-            );
         }
     }
 
     public function down(): void
     {
         throw new Error('Downgrade is prohibited!');
+    }
+
+    protected function exec(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
     }
 }
