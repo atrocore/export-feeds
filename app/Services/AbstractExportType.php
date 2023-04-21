8826c0a786b6cc6c137a2b3d8f54e340062b3ad7
r.ratsun <r.ratsun@gmail.com>
2023-04-21 10:06:00 +0300

    .

--- app/Services/AbstractExportType.php
+++ app/Services/AbstractExportType.php
@@ -81,6 +81,10 @@ abstract class AbstractExportType extends Base
                 $row['exportBy'] = ['id'];
             }
 
+            if (in_array($data['type'], ['extensibleEnum', 'extensibleMultiEnum'])) {
+                $row['exportBy'] = ['name'];
+            }
+
             if ($data['type'] === 'linkMultiple') {
                 $row['exportIntoSeparateColumns'] = false;
                 $row['offsetRelation'] = 0;
