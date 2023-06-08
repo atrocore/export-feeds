6c6ac91ddbaf154ef0f2f0b007794f5138a65a57
Raoul Dzoukou <rd@atrocore.com>
2023-06-08 10:51:19 +0100

    commit

--- app/Migrations/V1Dot7Dot1.php
+++ app/Migrations/V1Dot7Dot1.php
@@ -29,12 +29,14 @@ class V1Dot7Dot1 extends Base
     public function up(): void
     {
         $this->getPDO()->exec("ALTER TABLE export_feed ADD code VARCHAR(255) DEFAULT NULL UNIQUE COLLATE `utf8mb4_unicode_ci`");
-        $this->getPDO()->exec("CREATE UNIQUE INDEX UNIQ_8007192577153098EB4823 ON export_feed (code, deleted)");
+        $this->getPDO()->exec("CREATE UNIQUE INDEX UNIQ_5F1724077153098EB3B4E33 ON export_feed (code, deleted)");
     }
 
     public function down(): void
     {
-        $this->getPDO()->exec("ALTER TABLE export_feed drop column code");
+        $this->getPDO()->exec("DROP INDEX code ON export_feed");
+        $this->getPDO()->exec("DROP INDEX UNIQ_5F1724077153098EB3B4E33 ON export_feed");
+        $this->getPDO()->exec("ALTER TABLE export_feed DROP code");
     }
 
 }
