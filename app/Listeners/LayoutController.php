d641acb03c176074966bb7b0328b5c40b593be38
r.ratsun <r.ratsun@gmail.com>
2023-06-08 11:54:45 +0300

    .

--- app/Listeners/LayoutController.php
+++ app/Listeners/LayoutController.php
@@ -23,8 +23,8 @@ declare(strict_types=1);
 namespace Export\Listeners;
 
 use Espo\Core\Utils\Json;
-use Treo\Core\EventManager\Event;
-use Treo\Listeners\AbstractListener;
+use Espo\Core\EventManager\Event;
+use Espo\Listeners\AbstractListener;
 
 class LayoutController extends AbstractListener
 {
