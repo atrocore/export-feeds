02757d6a7f8adfaea27eb496b2b0bce62916a9ad
Raoul Dzoukou <rd@atrocore.com>
2023-08-30 08:50:18 +0100

    .

--- app/Migrations/V1Dot7Dot21.php
+++ app/Migrations/V1Dot7Dot21.php
@@ -29,6 +29,7 @@ class V1Dot7Dot21 extends Base
     public function up(): void
     {
         $this->getPDO()->exec("UPDATE export_configurator_item SET attribute_value='valueUnit' WHERE attribute_value='valueUnitId'");
+        $this->getPDO()->exec("UPDATE export_configurator_item SET attribute_value='value' WHERE attribute_value='valueWithUnit'");
     }
 
     public function down(): void
