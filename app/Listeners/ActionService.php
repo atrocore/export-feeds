47a2ce24228c75e8a2cd60298f2d7c9f8ff867ce
Roman Ratsun <rr@atrocore.com>
2024-01-12 07:30:24 +0000

    .

---
+++ app/Listeners/ActionService.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Export\Listeners;
+
+use Atro\Core\EventManager\Event;
+use Atro\Listeners\AbstractListener;
+use Espo\ORM\Entity;
+
+class ActionService extends AbstractListener
+{
+    public function prepareEntityForOutput(Event $event): void
+    {
+        /** @var Entity $entity */
+        $entity = $event->getArgument('entity');
+
+        if (!empty($entity->get('exportFeedId'))) {
+            $exportFeed = $this->getEntityManager()->getRepository('ExportFeed')->get($entity->get('exportFeedId'));
+            if (!empty($exportFeed)) {
+                $entity->set('exportFeedName', $exportFeed->get('name'));
+            }
+        }
+    }
+}
