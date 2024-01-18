d8ae013630e3a3cc62c8c8f51524134bc8721def
Roman Ratsun <rr@atrocore.com>
2024-01-18 12:28:11 +0000

    .

--- client/modules/export/src/views/action/fields/export-feed.js
+++ client/modules/export/src/views/action/fields/export-feed.js
@@ -15,9 +15,14 @@ Espo.define('export:views/action/fields/export-feed', 'views/fields/link', Dep =
         setup() {
             Dep.prototype.setup.call(this);
 
-            this.listenTo(this.model, 'change:type', () => {
-                if (this.model.get('type') === 'export'){
-                    this.model.set('payload', '{"where":[{"type":"in","attribute":"id","value":{{ sourceEntitiesIds|json_encode|raw}}}]}');
+            this.listenTo(this.model, 'change:type change:usage', () => {
+                if (this.model.get('type') === 'export') {
+                    if (this.model.get('usage') === 'record') {
+                        this.model.set('payload', '{"where":[{"type":"in","attribute":"id","value":{{ sourceEntitiesIds|json_encode|raw}}}]}');
+                    }
+                    if (this.model.get('usage') === 'entity') {
+                        this.model.set('payload', '');
+                    }
                 }
             });
         },
